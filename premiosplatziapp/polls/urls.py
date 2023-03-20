18185196ac72742af04cbbe139d7825d49779251
Kirill <kirillruso10@gmail.com>
2023-03-20 20:19:34 +0100

    Creamos las primeras templates del proyecto

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # Ex: /polls/3
+    path("<int:question_id>/detail", views.detail, name="detail"),
+    # Ex: /polls/3/results
+    path("<int:question_id>/results", views.results, name="results"),
+    # Ex: /polls/3/vote
+    path("<int:question_id>/vote", views.vote, name="vote"),
 ]
