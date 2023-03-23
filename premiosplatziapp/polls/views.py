5219e5bf4843199b7850788ca8745a431529ecfa
Kirill <kirillruso10@gmail.com>
2023-03-23 23:41:36 +0100

    Creamos la funcionalidad para votar los cursos y visualizar los votos

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,7 @@ def index(request):
 
 
 def detail(request, question_id):
+    # get_object_or_404 Sirve para traernos las preguntas o devolver un error 404
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {
         "question": question
@@ -18,8 +20,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
+    question =  get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Es una buena practica redirigir al usuario usando HttpResponseRedirect
+        # reverse sirve al igual que html pero en python para no hardcodear una url
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
