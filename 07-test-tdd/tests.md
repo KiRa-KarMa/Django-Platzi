5d9d1b8983f5edd23efb3cb712c3ce5796296b36
Kirill <kirillruso10@gmail.com>
2023-03-25 14:17:10 +0100

    Realizamos tests para la función was_published_recently()

---
+++ 07-test-tdd/tests.md
@@ -0,0 +1,14 @@
+
+
+# TESTS
+
+Los test de nuestro codigo son muy importantes por estas razones:
+
+    - Si tenemos muchas líneas de código, encontrar un error puede ser catastrófico.
+    - Nos hace ver más profesionales.
+    - Nos permite trabajar mejor en equipo
+
+# TDD (Test-Driven Development)
+
+Es una práctica de programación que consiste en escribir primero las pruebas (generalmente unitarias), despues escribir el codigo fuente
+que pase la prueba satisfactoria y por último, refactorizar el codigo escrito.
--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from polls.models import Question
 
-# Create your tests here.
+# Testeamos o Modelos o Vistas
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", pub_date=time)
+        # Afirma si la ejecución de esa función devuelve False
+        self.assertIs(future_question.was_published_recently(), False)
